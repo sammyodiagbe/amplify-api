9d5aaa76487126c5e7949dfd727f373586719ab1
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2021-07-09 03:03:53 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import { API } from "aws-amplify";
 
 function App() {
+  // useEffect(() => {
+
+  // }, [])
+  const getApi = async () => {
+    const data = await API.get("todos", "/todos");
+    console.log(data);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Calling api, using rest </h1>
+        <hr />
+        <button onClick={getApi}>call backend api right now</button>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
